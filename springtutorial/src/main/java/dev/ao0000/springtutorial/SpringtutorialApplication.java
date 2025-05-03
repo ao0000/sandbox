3f67b05f689059405b5a211609f565591b244c2e
ao0000 <ao0000dev@gmail.com>
2025-05-03 11:55:58 +0900

    feat: route endpoint for hello string

--- springtutorial/src/main/java/dev/ao0000/springtutorial/SpringtutorialApplication.java
+++ springtutorial/src/main/java/dev/ao0000/springtutorial/SpringtutorialApplication.java
@@ -1,13 +1,24 @@
 package dev.ao0000.springtutorial;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringtutorialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringtutorialApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(SpringtutorialApplication.class);
+		springApplication.setBannerMode(Banner.Mode.OFF);
+		springApplication.run(args);
 	}
 
+	@RequestMapping("/")
+	public String index() {
+		return "Hello, Spring Boot3!!!";
+	}
 }
