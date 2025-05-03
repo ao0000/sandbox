3bb40d25ab085e66d4e52948e689791a93f32d2d
ao0000 <ao0000dev@gmail.com>
2025-05-03 14:35:05 +0900

    feat: redirect and forward

--- springtutorial/src/main/java/dev/ao0000/springtutorial/HelloController.java
+++ springtutorial/src/main/java/dev/ao0000/springtutorial/HelloController.java
@@ -2,8 +2,6 @@ package dev.ao0000.springtutorial;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -51,39 +49,55 @@ public class HelloController {
 //        return model;
 //    }
 
+    //    @GetMapping("/")
+//    public ModelAndView index(ModelAndView modelAndView) {
+//        modelAndView.setViewName("index");
+//        modelAndView.addObject("msg", "フォームを送信ください。");
+//        return modelAndView;
+//    }
+//
+//    @PostMapping("/")
+//    public ModelAndView form(
+//            @RequestParam(value = "check1", required = false) boolean check1,
+//            @RequestParam(value = "radio1", required = false) String radio1,
+//            @RequestParam(value = "select1", required = false) String select1,
+//            @RequestParam(value = "select2", required = false) String[] select2,
+//            ModelAndView model
+//    ) {
+//        String res = "";
+//        try {
+//            res = "check1: " + check1 +
+//                    " radio1: " + radio1 +
+//                    " select1: " + select1 +
+//                    "\nselect2: ";
+//        } catch (Exception e) {
+//        }
+//        try {
+//            res += select2[0];
+//            for (int i = 1; i < select2.length; i++) {
+//                res += ", " + select2[i];
+//            }
+//        } catch (Exception e) {
+//            res += "null";
+//        }
+//        model.addObject("msg", res);
+//        model.setViewName("index");
+//        return model;
+//    }
     @GetMapping("/")
     public ModelAndView index(ModelAndView modelAndView) {
         modelAndView.setViewName("index");
-        modelAndView.addObject("msg", "フォームを送信ください。");
+        modelAndView.addObject("msg", "HelloController/indexのページです。");
         return modelAndView;
     }
 
-    @PostMapping("/")
-    public ModelAndView form(
-            @RequestParam(value = "check1", required = false) boolean check1,
-            @RequestParam(value = "radio1", required = false) String radio1,
-            @RequestParam(value = "select1", required = false) String select1,
-            @RequestParam(value = "select2", required = false) String[] select2,
-            ModelAndView model
-    ) {
-        String res = "";
-        try {
-            res = "check1: " + check1 +
-                    " radio1: " + radio1 +
-                    " select1: " + select1 +
-                    "\nselect2: ";
-        } catch (Exception e) {
-        }
-        try {
-            res += select2[0];
-            for (int i = 1; i < select2.length; i++) {
-                res += ", " + select2[i];
-            }
-        } catch (Exception e) {
-            res += "null";
-        }
-        model.addObject("msg", res);
-        model.setViewName("index");
-        return model;
+    @GetMapping("/other")
+    public String other() {
+        return "redirect:/";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "forward:/";
     }
 }
