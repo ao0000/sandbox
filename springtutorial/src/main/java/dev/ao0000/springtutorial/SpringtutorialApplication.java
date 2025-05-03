0b994a3827c58d056138f417c9214c41f570ef6a
ao0000 <ao0000dev@gmail.com>
2025-05-03 12:00:43 +0900

    feat: add application runner program

--- springtutorial/src/main/java/dev/ao0000/springtutorial/SpringtutorialApplication.java
+++ springtutorial/src/main/java/dev/ao0000/springtutorial/SpringtutorialApplication.java
@@ -1,27 +1,30 @@
 package dev.ao0000.springtutorial;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class SpringtutorialApplication implements CommandLineRunner {
+public class SpringtutorialApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(SpringtutorialApplication.class);
-		springApplication.setBannerMode(Banner.Mode.OFF);
-		springApplication.run(args);
-	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("---------------------------------------");
-		System.out.println("| This is CommnadLine Runner program. |");
-		System.out.println("---------------------------------------");
-		System.out.println("["+String.join(", ", args)+"]");
-		System.out.println();
-	}
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(SpringtutorialApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("---------------------------------------");
+        System.out.println("| This is Application Runner program. |");
+        System.out.println("---------------------------------------");
+        System.out.println(args.getOptionNames());
+        System.out.println(args.getNonOptionArgs());
+        System.out.println(Arrays.asList(args.getSourceArgs()));
+    }
 }
