d43b037b077e3adaadca38199b4c0271930efd81
ao0000 <ao0000dev@gmail.com>
2025-05-03 13:17:09 +0900

    feat: add radio and select form

--- springtutorial/src/main/java/dev/ao0000/springtutorial/HelloController.java
+++ springtutorial/src/main/java/dev/ao0000/springtutorial/HelloController.java
@@ -1,8 +1,8 @@
 package dev.ao0000.springtutorial;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -36,17 +36,53 @@ public class HelloController {
 //        return mav;
 //    }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index(ModelAndView model) {
-        model.addObject("msg", "名前をかいてください。");
-        model.setViewName("index");
-        return model;
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    public ModelAndView index(ModelAndView model) {
+//        model.addObject("msg", "名前をかいてください。");
+//        model.setViewName("index");
+//        return model;
+//    }
+//
+//    @RequestMapping(value = "/", method = RequestMethod.POST)
+//    public ModelAndView form(@RequestParam("text1") String str, ModelAndView model) {
+//        model.addObject("msg", "こんにちは、" + str + "さん");
+//        model.addObject("value", str);
+//        model.setViewName("index");
+//        return model;
+//    }
+
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        modelAndView.addObject("msg", "フォームを送信ください。");
+        return modelAndView;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public ModelAndView form(@RequestParam("text1") String str, ModelAndView model) {
-        model.addObject("msg", "こんにちは、" + str + "さん");
-        model.addObject("value", str);
+    @PostMapping("/")
+    public ModelAndView form(
+            @RequestParam(value = "check1", required = false) boolean check1,
+            @RequestParam(value = "radio1", required = false) String radio1,
+            @RequestParam(value = "select1", required = false) String select1,
+            @RequestParam(value = "select2", required = false) String[] select2,
+            ModelAndView model
+    ) {
+        String res = "";
+        try {
+            res = "check1: " + check1 +
+                    " radio1: " + radio1 +
+                    " select1: " + select1 +
+                    "\nselect2: ";
+        } catch (Exception e) {
+        }
+        try {
+            res += select2[0];
+            for (int i = 1; i < select2.length; i++) {
+                res += ", " + select2[i];
+            }
+        } catch (Exception e) {
+            res += "null";
+        }
+        model.addObject("msg", res);
         model.setViewName("index");
         return model;
     }
