ba0f5ac615019b0bb4568c73742d311a28769b75
ao0000 <ao0000dev@gmail.com>
2025-05-03 13:05:39 +0900

    feat: form get and psot

--- springtutorial/src/main/java/dev/ao0000/springtutorial/HelloController.java
+++ springtutorial/src/main/java/dev/ao0000/springtutorial/HelloController.java
@@ -1,8 +1,9 @@
 package dev.ao0000.springtutorial;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,15 +24,30 @@ public class HelloController {
 //        return "index";
 //    }
 
-    @RequestMapping("/{num}")
-    public ModelAndView index(@PathVariable int num, ModelAndView mav) {
-        int total = 0;
-        for (int i = 1; i <= num; i++) {
-            total += i;
-        }
-        mav.addObject("msg", num + "までの合計を計算します。");
-        mav.addObject("content", "total: " + total);
-        mav.setViewName("index");
-        return mav;
+//    @RequestMapping("/{num}")
+//    public ModelAndView index(@PathVariable int num, ModelAndView mav) {
+//        int total = 0;
+//        for (int i = 1; i <= num; i++) {
+//            total += i;
+//        }
+//        mav.addObject("msg", num + "までの合計を計算します。");
+//        mav.addObject("content", "total: " + total);
+//        mav.setViewName("index");
+//        return mav;
+//    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView model) {
+        model.addObject("msg", "名前をかいてください。");
+        model.setViewName("index");
+        return model;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ModelAndView form(@RequestParam("text1") String str, ModelAndView model) {
+        model.addObject("msg", "こんにちは、" + str + "さん");
+        model.addObject("value", str);
+        model.setViewName("index");
+        return model;
     }
 }
