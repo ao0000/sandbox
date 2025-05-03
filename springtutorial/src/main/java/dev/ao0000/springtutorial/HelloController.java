04a69442264d469d646c957cf22aad8380d563e3
ao0000 <ao0000dev@gmail.com>
2025-05-03 12:57:12 +0900

    feat: fix ModelAndView

--- springtutorial/src/main/java/dev/ao0000/springtutorial/HelloController.java
+++ springtutorial/src/main/java/dev/ao0000/springtutorial/HelloController.java
@@ -3,14 +3,35 @@ package dev.ao0000.springtutorial;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/{temp}")
-    public String index(@PathVariable String temp) {
-        return switch (temp) {
-            case "index" -> "index";
-            default -> "other";
-        };
+//   @RequestMapping("/")
+//   public String index(Model model) {
+//       model.addAttribute("msg", "これはコントローラに用意したメッセージです。");
+//       return "index";
+//   }
+
+//    @RequestMapping("/{num}")
+//    public String index(@PathVariable int num, Model model) {
+//        int res = 0;
+//        for (int i = 1; i <= num; i++) {
+//            res += i;
+//        }
+//        model.addAttribute("msg", "total: " + res);
+//        return "index";
+//    }
+
+    @RequestMapping("/{num}")
+    public ModelAndView index(@PathVariable int num, ModelAndView mav) {
+        int total = 0;
+        for (int i = 1; i <= num; i++) {
+            total += i;
+        }
+        mav.addObject("msg", num + "までの合計を計算します。");
+        mav.addObject("content", "total: " + total);
+        mav.setViewName("index");
+        return mav;
     }
 }
